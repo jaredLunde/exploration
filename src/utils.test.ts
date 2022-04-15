c9cac78cec5dc4df4861446b3d0247f0f575cfa1
Jared Lunde <jared.lunde@gmail.com>
2022-04-15 17:34:24 -0600

    test: add more tests (#8)

--- src/utils.test.ts
+++ src/utils.test.ts
@@ -1,3 +1,4 @@
+import { mergeProps } from ".";
 import { shallowEqual } from "./utils";
 
 describe("shallowEqual()", () => {
@@ -31,5 +32,49 @@ describe("shallowEqual()", () => {
 
   it("should return false for object/null", () => {
     expect(shallowEqual(null, {})).toBe(false);
+  });
+});
+
+describe("mergeProps()", () => {
+  it("should chain callbacks", () => {
+    const handleA = jest.fn();
+    const handleB = jest.fn();
+    const merged = mergeProps([{ onChange: handleA }, { onChange: handleB }]);
+
+    merged.onChange("foo");
+    expect(handleA).toHaveBeenCalledWith("foo");
+    expect(handleB).toHaveBeenCalledWith("foo");
+  });
+
+  it("should merge class names", () => {
+    const merged = mergeProps([{ className: "foo" }, { className: "bar" }]);
+
+    expect(merged.className).toBe("foo bar");
+  });
+
+  it("should merge unsafe class names", () => {
+    const merged = mergeProps([
+      // eslint-disable-next-line camelcase
+      { UNSAFE_className: "foo" },
+      // eslint-disable-next-line camelcase
+      { UNSAFE_className: "bar" },
+    ]);
+
+    expect(merged.UNSAFE_className).toBe("foo bar");
+  });
+
+  it("should merge styles", () => {
+    const merged = mergeProps([
+      { style: { color: "red" } },
+      { style: { backgroundColor: "blue" } },
+    ]);
+
+    expect(merged.style).toEqual({ color: "red", backgroundColor: "blue" });
+  });
+
+  it("should merge styles 2", () => {
+    const merged = mergeProps([{}, { style: { backgroundColor: "blue" } }]);
+
+    expect(merged.style).toEqual({ backgroundColor: "blue" });
   });
 });
